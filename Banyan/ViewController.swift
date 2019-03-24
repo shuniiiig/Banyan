84a03bb925e9c06bc9d1a6e77443a7349141b6e2
rioyuzu1127 <henry12.henry14+github@gmail.com>
2019-03-24 21:29:44 +0900

    first commit

---
+++ Banyan/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Banyan
+//
+//  Created by 井草俊輔 on 2019/03/24.
+//  Copyright © 2019 井草俊輔. All rights reserved.
+//
+
+import UIKit
+
+open class ViewController: UIViewController, UIScrollViewDelegate {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
