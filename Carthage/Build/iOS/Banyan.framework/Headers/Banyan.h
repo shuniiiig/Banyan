2b3f574154a9416e00daa377f9950c7416dcd594
shuniiiig <henry12.henry14+github@gmail.com>
2019-11-28 00:18:41 +0900

    delete files

---
+++ Carthage/Build/iOS/Banyan.framework/Headers/Banyan.h
@@ -0,0 +1,19 @@
+//
+//  Banyan.h
+//  Banyan
+//
+//  Created by 井草俊輔 on 2019/04/01.
+//  Copyright © 2019 井草俊輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Banyan.
+FOUNDATION_EXPORT double BanyanVersionNumber;
+
+//! Project version string for Banyan.
+FOUNDATION_EXPORT const unsigned char BanyanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Banyan/PublicHeader.h>
+
+
